0d790f8e2b08268841d0366688ac7b8290d17f7b
Salty9 <70324504+Salty9@users.noreply.github.com>
2024-03-05 01:25:03 +0600

    Add files via upload

--- iMain.cpp
+++ iMain.cpp
@@ -2,16 +2,18 @@
 # include <stdlib.h>
 # include <time.h>
  
-int screen_height, screen_width, window_height, window_width;
+int window_height = 790, window_width = 1440;
 
 int grid_initialized = 0;
 double start_x = 50, start_y = 50, size = 50, padding = 4, text_offset = 10;
-int rows = 10, cols = 15; // 10, 15 before
+int rows = 10, cols = 15, uncovered_count = 0; // 10, 15 before
 int mine_count = 20, page = 0, marked_counter = 0;
 int life_height = 55+40; // image + offset
 
-int max_lives = 9;
+int max_lives = 1;
 int lives = max_lives; 
+int loss_music = 0;
+
 
 //function iDraw() is called again and again by the system.
 
@@ -34,7 +36,7 @@ Effect effects[] = {Effect("gah_sound.wav", "gah.bmp", 1.3), Effect("explosion_3
 
 struct Cell
 {
-	int uncovered = 0, mine = 0, clue = 0, marker = 0, sound_played = 0, sound_started = 0, chosen_effect = -1, sound_start_time = 0;
+	int uncovered = 0, mine = 0, clue = 0, marker = 0, sound_played = 0, sound_started = 0, chosen_effect = 0, sound_start_time = 0;
 	int left_x, left_y, sz;
 
 	Cell(double xx = 0, double yy = 0, double size = 0){
@@ -161,7 +163,6 @@ void clear_around_zeros(){
 	
 }
 
-
 void draw_grid(){
 	// iLine take double as input
 
@@ -181,7 +182,7 @@ void draw_grid(){
 		shuffle_mines();
 		for(int i = 0; i < mine_count; i++){
 			cells[ mines[i].x ][ mines[i].y ].mine = 1; 
-			cells[ mines[i].x ][ mines[i].y ].chosen_effect = rand() % 2;
+			cells[ mines[i].x ][ mines[i].y ].chosen_effect = rand() % 2; // CHANGE IF EFFECTS ARE ADDED !!!!!
 		}
 
 		for(int i = 0; i < rows; i++){
@@ -195,8 +196,6 @@ void draw_grid(){
 		return;
 	}
 	
-	
-
 	for(int i = 0; i < rows; i++){
 		for(int j = 0; j < cols; j++){
 			cells[i][j].draw_cell();
@@ -227,26 +226,42 @@ void side_stuff(){
 
 void board_dimensions(int p){
 	if(p == 5){
-		rows = 9; cols = 9; mine_count = 10; size = 50;
+		rows = 9; cols = 9; mine_count = 10; size = 50; max_lives = 3; 
 	}else if(p == 6){
-		rows = 16, cols = 16, mine_count = 40, size = 40;
+		rows = 16, cols = 16, mine_count = 40, size = 40; max_lives = 5;
 	}else if(p == 7){
-		rows = 16; cols = 30; mine_count = 99; size = 30;
+		rows = 16; cols = 30; mine_count = 99; size = 30; max_lives = 9;
 	}
+	lives = max_lives;
 	start_x = (window_width - cols*size - 510)/2.0;
 	start_y = (window_height - rows*size - life_height)/2.0;
+	uncovered_count = 0;
+}
+
+bool win_check(){
+	int res = 0;
+	for(int i = 0; i < rows; i++){
+		for(int j = 0; j < cols; j++){
+			if(!cells[i][j].mine && cells[i][j].uncovered) res++;
+		}
+	}
+	return (res == rows*cols - mine_count);
 }
 
 void event_handler(){
-	if(lives == 0){
-		page = 2; // 2 for loss
-		return;
+	if(page == 1){
+		if(lives == 0){
+			page = 2; // 2 for loss
+			return;
+		}
+		if(win_check()){
+			page = 3; // 3 for win
+			return;
+		}
 	}
-	if(marked_counter == mine_count){
-		page = 3; // 3 for win
-		return;
-	}
+
 }
+
 
 void iDraw() {
 
@@ -261,9 +276,16 @@ void iDraw() {
 		side_stuff();
 	}else if(page == 2){
 		iShowBMP(0, 0, "lost_page.bmp");
+		iShowBMP((window_width - 715)/2.0, 200, "dio.bmp"); // 715 x 413 
+		if(!loss_music){
+			loss_music = 1;
+			PlaySound(NULL, 0, 0); // stops any sound playing before
+			PlaySound(TEXT("bites_dust.wav"), 0, SND_LOOP | SND_ASYNC);
+		}
 	}else if(page == 3){
 		iShowBMP(0, 0, "win_page.bmp");
 	}else if(page == 4){
+	
 		iShowBMP(0, 0, "levels.bmp");
 	}else if(page == 5){
 		iShowBMP(0, 0, "levels_beginner.bmp");
@@ -271,6 +293,8 @@ void iDraw() {
 		iShowBMP(0, 0, "levels_intermediate.bmp");
 	}else if(page == 7){
 		iShowBMP(0, 0, "levels_expert.bmp");
+	}else if(page == 8){
+		iShowBMP(0, 0, "instructions.bmp");		
 	}
 }
 
@@ -279,77 +303,74 @@ void iMouseMove(int mx, int my) {
 
 }
 
-
-
 void iMouse(int button, int state, int mx, int my) {
-	// printf("%d %d\n", mx, my);
+	printf("%d %d\n", mx, my);
 
-	if(page >= 4){
-		if(mx >= 82 && mx <= 348 && my >= 340 && my <= 586){
+	if(page == 0){
+		if(mx >= 1093 && mx <= 1322 && my >= 489 && my <= 538){
+			page = 4; return;
+		}else if(mx >= 1036 && mx <= 1371 && my >= 370 && my <= 421){
+			page = 8; return; // instruction page
+		}else if(mx >= 1149 && mx <= 1253 && my >= 260 && my <= 311){
+			exit(0);
+		}
+	}else if((page == 4 || page == 5 || page == 6 || page == 7)  && button == GLUT_LEFT_BUTTON && state == GLUT_DOWN){
+		if(mx >= 163 && mx <= 429 && my >= 378 && my <= 620){
 			page = 5;
-		}else if(mx >= 464 && mx <= 812 && my >= 340 && my <= 586){
+		}else if(mx >= 421 && mx <= 829 && my >= 376 && my <= 620){
 			page = 6;
-		}else if(mx >= 935 && mx <= 1195 && my >= 340 && my <= 586){
+		}else if(mx >= 1015 && mx <= 1275 && my >= 377 && my <= 621){
 			page = 7;
-		}else if(mx >= 493 && mx <= 760 && my >= 137 && my <= 210){
+		}else if(mx >= 575 && mx <= 841 && my >= 171 && my <= 242){
 			board_dimensions(page);
 			page = 1;
 		}
-	}
+	}else if(page == 1){
+		int i = compress_y_to_i(my);
+		int j = compress_x_to_j(mx);
 
-	int i = compress_y_to_i(my);
-	int j = compress_x_to_j(mx);
+		if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
 
-	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-
-		if(0 <= i && i < rows && 0 <= j && j < cols){	
-			if(cells[i][j].clue == 0 && !cells[i][j].mine) {
-				clear_zeros(i, j);
-				clear_around_zeros();
+			if(0 <= i && i < rows && 0 <= j && j < cols){	
+				if(cells[i][j].clue == 0 && !cells[i][j].mine) {
+					clear_zeros(i, j);
+					clear_around_zeros();
+				}
+				cells[i][j].uncovered = 1; // i = y aixs, j is x axis
 			}
-			cells[i][j].uncovered = 1; // i = y aixs, j is x axis
 		}
-	}
-	if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
-		if(0 <= i && i < rows && 0 <= j && j < cols && !cells[i][j].uncovered){
-			cells[i][j].marker ^= 1;
+		if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
+			if(0 <= i && i < rows && 0 <= j && j < cols && !cells[i][j].uncovered){
+				cells[i][j].marker ^= 1;
 
-			if(cells[i][j].marker) marked_counter++;
-			else marked_counter--;
+				if(cells[i][j].marker) marked_counter++;
+				else marked_counter--;
+			}
+		}
+	}else if(page == 2){
+		if(mx >= 1243 && mx <= 1401 && my >= 37 && my <= 156){
+			PlaySound(NULL, 0, 0);
+			page = 0;
+			grid_initialized = 0;
+			loss_music = 0;
+		}
+	}else if(page == 8){
+		if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN){
+			page = 0;
 		}
 	}
+
 }
 
 void iKeyboard(unsigned char key) {
-	if (key == 'q') {
-		exit(0);
-	}
-
-	if(key == '\r' && page == 0){
-		page = 4; // to levels
-	}
+	if (key == 'q') exit(0); 
 }
-
 
 void iSpecialKeyboard(unsigned char key) {
-
-	if (key == GLUT_KEY_END) {
-		exit(0);
-	}
+	if (key == GLUT_KEY_END) exit(0);
 }
 
-
 int main() {
-	//place your own initialization codes here.
-
- 	screen_height = GetSystemMetrics(SM_CYSCREEN);
-    screen_width = GetSystemMetrics(SM_CXSCREEN);
-
-	// window_height = (screen_height*3)/4;
-	// window_width = (screen_width*3)/4;
-	window_height = 790;
-	window_width = 1440;
-
 	iInitialize(window_width, window_height, "Minesweeper");
 	return 0;
 }
